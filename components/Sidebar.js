479bdb88d6ed3ce9c20bf39e2450244f2d87321f
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-02-07 21:39:25 -0300

    Criando ambiente de desenvolvimento

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default function Sidebar() {
+    return(
+        <Container>
+            <Header>Sidebar</Header>
+        </Container>
+    )
+}
+
+const Container = styled.div``
+
+const Header = styled.div``
