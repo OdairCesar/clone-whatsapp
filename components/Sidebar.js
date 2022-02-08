d8485582bd17f21d8d4e8e25384bdcc963acd62e
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-02-07 23:47:55 -0300

    Criando o login do clone

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,24 @@
-import styled from 'styled-components';
-import { Avatar, IconButton, Button } from '@material-ui/core';
-import ChatIcon from "@material-ui/icons/Chat";
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import SearchIcon from '@material-ui/icons/Search';
+import styled from 'styled-components'
+import { Avatar, IconButton, Button } from '@material-ui/core'
+import ChatIcon from "@material-ui/icons/Chat"
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import SearchIcon from '@material-ui/icons/Search'
+import * as EmailValidator from 'email-validator'
 
 function Sidebar() {
+  const createChat = () => {
+    const input = prompt(
+      "Por favor digite o endereço de e-mail de quem deseja conversar."
+    )
+
+    if(!input) return null
+    
+    if(EmailValidator.validate(input)) {
+      
+    }
+
+  }
+
   return (
     <Container>
       <Header>
@@ -25,28 +39,28 @@ function Sidebar() {
         <SearchInput placeholder='Procure uma conversa'/>
       </Search>
 
-      <SidebarButton>
+      <SidebarButton onClick={createChat}>
         Iniciar nova conversa
       </SidebarButton>
     </Container>
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div``
 
 const Search = styled.div`
   display: flex;
   align-items:  center;
   padding: 20px;
   border-radius: 2px;
-`;
+`
 
 const SearchInput = styled.input`
   outline-width: 0;
   border: none;
   flex: 1;
   font-size: 0.9rem;
-`;
+`
 
 const SidebarButton = styled(Button)`
   width: 100%;
@@ -55,7 +69,7 @@ const SidebarButton = styled(Button)`
     border-top: 1px solid whitesmoke;
     border-bottom: 1px solid whitesmoke;
   }
-`;
+`
 
 const Header = styled.div`
   position: sticky;
@@ -71,15 +85,15 @@ const Header = styled.div`
   border-bottom: 1px solid whitesmoke;
   background-color: #fff;
   z-index: 1;
-`;
+`
 
 const UserAvatar = styled(Avatar)`
   cursor: pointer;
   :hover{
     opacity: 0.8;
   }
-`;
+`
 
-const IconsContainer = styled.div``;
+const IconsContainer = styled.div``
 
-export default Sidebar;
+export default Sidebar
